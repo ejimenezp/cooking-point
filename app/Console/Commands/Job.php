dd490a347808ec1a57bbd3152c0749989cf2c24e
ejimenezp <eduardo@cookingpoint.es>
2016-09-27 20:06:13 +0200

    reminder command
    new files

---
+++ app/Console/Commands/Job.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use \DB;
+use \DateTime;
+use \DateTimeZone;
+use Log;
+
+class Job {
+
+	protected $jobname;
+	protected $condition;
+	protected $now;
+	protected $result;
+
+	function __construct($jobname)
+	{
+
+		$this->jobname = $jobname;
+		$this->now = new DateTime(null, new DateTimeZone('Europe/Madrid'));
+
+	}
+
+	function query()
+	{	
+		$this->set_condition();
+		// Log::info($this->jobname . "query is = " . $this->condition);
+
+		if(!$r = DB::select($this->condition)) {
+			Log::info('No rows for ' . $this->jobname);
+			return false;
+		}
+		$this->result = $r;
+		return true;
+	}
+
+	function exec()
+	{
+		foreach ($this->result as $item) {
+			$this->action($item);
+		}
+	}
+
+
+} 
---
+++ app/Console/Commands/Reminder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use \DateTime;
+use Log;
+
+use App\Http\Controllers\Legacy\LegacyMail;
+use App\Http\Controllers\Legacy\LegacyModel;
+
+
+class Reminder extends Job {
+
+	function __construct()
+	{
+		parent::__construct("<Reminder>");
+	}
+
+	protected function set_condition()
+	{
+		$today = $this->now->format('Y-m-d');
+		$in2days = $this->now->modify('+2 days')->format('Y-m-d');
+
+		$this->condition = "SELECT * from legacy_bookings where 
+			status = 'PE' and 
+			crm <> 'NO' and 
+			crm <> 'RE' and
+			activityDate >= '{$today}' and
+			activityDate <= '{$in2days}'";
+	}
+
+	protected function action($booking) {
+		// para testing solo escribe en el log
+		Log::info('Cumple con la condición ' . $booking->name);
+
+	 	LegacyMail::mail_to_user(get_object_vars($booking), "legacy/reminder");
+	 	$booking->crm = "RE";
+	 	LegacyModel::update_admin_booking(get_object_vars($booking));
+	}
+}
+
