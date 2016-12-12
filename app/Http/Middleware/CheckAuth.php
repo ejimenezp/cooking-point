edf557e6c93eb5180f32829153ec354e7aea113f
ejimenezp <eduardo@cookingpoint.es>
2016-12-12 17:09:07 +0100

    check user's cookie exists
    .

--- app/Http/Middleware/CheckAuth.php
+++ app/Http/Middleware/CheckAuth.php
@@ -20,9 +20,13 @@ class CheckAuth
         if ($request->hasCookie('cpuser')) {
             $user_id = $request->cookie('cpuser');
             $user = Staff::find($user_id);
-            // Log::debug('usuario: ' . $user->name . ' role: ' . $user->auth_role);            
-            $request->merge(['user_name' => $user->name, 'user_role' => $user->auth_role]);
-            return $next($request);
+            if ($user) {
+                // Log::debug('usuario: ' . $user->name . ' role: ' . $user->auth_role);            
+                $request->merge(['user_name' => $user->name, 'user_role' => $user->auth_role]);
+                return $next($request);
+            } else {
+               return redirect()->route('login', ['redir' => $request->fullUrl()]);
+            }
         } else {
             return redirect()->route('login', ['redir' => $request->fullUrl()]);
         }
