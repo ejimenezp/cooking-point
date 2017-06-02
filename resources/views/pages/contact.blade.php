bd4f2d948f731a2b374b57301e978879423a3a61
ejimenezp <eduardo@cookingpoint.es>
2017-06-02 11:05:36 +0200

    add metro exit to voucher and page

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -17,7 +17,7 @@
 		<h1 class="header1">Contact</h1>
 		<p>Cooking Point is located in the Barrio de Huertas (or Barrio de las Letras, Letter's Quarter). An important place in history especially during the 16th-century Golden Age of Spanish Literature, Miguel de Cervantes and Lope de Vega both lived here. </p>
 
-		<p>This quarter is in the heart of Madrid and our school is within walking distance of Madrid landmarks like Museo del Prado or Puerta del Sol. The closest metro station is Anton Martin (line 1, light blue).</p>
+		<p>This quarter is in the heart of Madrid and our school is within walking distance of Madrid landmarks like Museo del Prado or Puerta del Sol. The closest metro station is Anton Martin (line 1, light blue), exit Amor de Dios St.</p>
 	</div>
 </div>
 
@@ -30,15 +30,17 @@
 	</div>
 	<div class="col-xs-3 col-sm-2 what">
  		<i class="fa fa-lg fa-map-marker"></i><br/>
+ 		<i class="fa fa-lg fa-subway"></i><br/>
  		<i class="fa fa-lg fa-clock-o"></i><br/>
  		<i class="fa fa-lg fa-envelope-o"></i><br/>
  		<i class="fa fa-lg fa-phone"></i><br/>
  	</div>
 	<div class="col-xs-9 no-link">
 		Calle de Moratin, 11  28014 Madrid (Spain)<br/>
+		Metro Anton Martin (Line 1), exit Amor de Dios St.<br/>
 		Monday to Saturday, 9:30 am - 9:30 pm<br/>
 		<a href="mailto:info@cookingpoint.es">info@cookingpoint.es</a><br/>
-		<a href="tel:(+34)910115154">(+34) 910 115 154<br/><br/>
+		<a href="tel:(+34)910115154">(+34) 910 115 154<br/>
 	</div>
 </div>
 
