8f80cbec11828b9979e95c4699354e0703784c3b
ejimenezp <eduardo@cookingpoint.es>
2017-08-03 20:20:14 +0200

    blog entry: fish stock

---
+++ resources/views/blog/fish-stock.blade.php
@@ -0,0 +1,55 @@
+@extends('masterlayout')
+
+@section('title', 'Fish Stock for Paella')
+@section('description', 'Make a fabulous fish stock for your Spanish paella')
+
+@section('content')
+
+<div class="row blog">
+	<div class="col-sm-12">
+		<h1 class="header1">Fish Stock for Paella</h1>
+
+<img class="img-responsive center-block" alt="paella fish stock" src="/images/blog/fish-stock.jpg" />
+
+<p></p>
+
+<p>One recurring question in our cooking classes is how we make the stock we use to make paella. We normally use fish stock when we make paella with any seafood or fish. This post gives you some guidelines on how to make good stock for paella. I say guidelines because there is not a single list of ingredients or invariable process.</p>
+
+<p> In many cookbooks it is called fish “fumet”, that literally means “aroma” in French. Although I recommend having it made before you start preparing the paella, you can also make it on the same cooking season.</p>
+
+<div class="header2">Ingredients</div>
+<p></p>
+<p>For 1.5 liters of stock:</p>
+<ul>
+	<li>0.5 - 1 kg white fish heads and/or bones (monkfish, hake, haddock, cod, flounder, sole, turbot…)</li>
+	<li>8 prawns</li>
+	<li>1.5 l water</li>
+	<li>1 onion</li>
+	<li>2 carrots</li>
+	<li>1 celery stalk</li>
+	<li>1 leek</li>
+	<li>1 bay leaf</li>	
+	<li>10-12 pepper corns</li>
+	<li>1 bundle of parsley</li>
+	<li>1 Tbsp salt</li>
+	<li>Extra virgin olive oil</li>
+</ul>
+
+<div class="header2">Method</div>
+<p></p>
+<ol>
+	<li>Peel the prawns to use only their heads and shells. Keep the flesh for other dishes.</li>
+	<li>Sautee the prawn heads and shells with a little of olive oil, using a pestle to crush them and get their juices out. Cook until they get pink.</li>
+	<li>Chop roughly the vegetables and add them to the saucepan. Sautee to sweat for a few minutes.</li>
+	<li>Add the fish heads and trimmings, water and spices. Bring to the boil, skim off the fat and simmer at low heat for 45 minutes.</li>
+	<li>Strain through a fine sieve and store. You can make portions and freeze it.</li>
+</ol>
+<p>You can add more seafood to make it tastier: mussels, clams, crabs…</p>
+
+<p>You can use similar process to make chicken stock. In that case, use a chicken quarter, sauteing before adding the vegetables. That way, you will get the pan flavoured with the skin fat.</p>
+
+	</div>
+</div>
+
+
+@stop
