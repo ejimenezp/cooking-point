02f43d8badd64000bc8bf968ae26f393c74caa87
ejimenezp <eduardo@cookingpoint.es>
2017-11-07 11:50:00 +0100

    welcome gift

--- resources/views/pages/paella.blade.php
+++ resources/views/pages/paella.blade.php
@@ -51,7 +51,8 @@
 		<strong>When:</strong> Monday to Saturday<br/>
 		<strong>Time:</strong> 10:00 am - 2:00 pm<br/>
 		<strong>Price: </strong>€70 adult / €35 children (5-12 yo)<br/>
-		<strong>Includes: </strong>market tour, cooking class, ingredients, recipes, lunch and drinks<br/>
+		<strong>Includes: </strong>market tour, cooking class, ingredients, recipes, <span style="text-decoration: underline;">welcome gift</span><span style="">*</span>, lunch and drinks<br/>
+		<span style="font-size:small;">Note: One gift per adult signed up for any class by Jan&nbsp31,&nbsp2018</span><br>
 	</div>	
 </div>
 
