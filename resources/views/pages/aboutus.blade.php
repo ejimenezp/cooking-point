6967bc65c43320ab7d2d401342bc44d7f2327159
ejimenezp <eduardo@cookingpoint.es>
2018-05-16 09:36:45 +0200

    new page: about us

---
+++ resources/views/pages/aboutus.blade.php
@@ -0,0 +1,54 @@
+@extends('masterlayout')
+
+@section('title', 'About Us')
+@section('description', 'We are 100% local, passionate about cooking and Spanish food culture.')
+
+@section('content')
+
+<div class="row">
+	<div class="col-sm-12">
+		<h1 class="header1">About Us</h1>
+		<p>We are 100% local, passionate about cooking and Spanish food culture.<br/><br/></p>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-6">
+		<div class="text-center">
+			<img class="text-center" src="/images/eduardo.jpg" alt="eduardo" />
+		</div>
+		<p class="header2">Eduardo</p>
+		<p>Born and raised in Madrid, Eduardo decided to quit his engineering and marketing career to teach the recipes he learned from his mum when he was a child. He believes gastronomy and the way people enjoy food say a lot about each country, feels like fish in water at the market or cooking something on the stove.
+</p>
+	</div>
+	<div class="col-sm-6">
+		<div class="text-center">
+			<img class="text-center" src="/images/laura.jpg" alt="laura" />
+		</div>
+		<p class="header2">Laura</p>
+		<p>Also 100% Madridian, Laura has been combining her two passions —translation and cooking—, during her whole life. She loves unveiling the stories and legends behind each recipe and will take you through a historical and gastronomical journey in every class. When she is not teaching, you can find her at the market getting supplies for her recently discovered recipes.</p>
+
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-6">
+		<div class="text-center">
+			<img class="text-center" src="/images/ligia.jpg" alt="lily" />
+		</div>
+		<p class="header2">Lily</p>
+		<p>Lily, born in Madrid, studied translation and cultural management. For the last several years, she’s been working as a cook and an expert in both Spanish and Latin American gastronomical cultural heritage. She’s a tireless world traveller and has lived in several countries, but when in her home city, she loves sharing all her tricks and curiosities about food and culture in Madrid, and Spain at large. She speaks Spanish, English, French, Portuguese, and Italian.</p>
+	</div>
+
+	<div class="col-sm-6">
+		<div class="text-center">
+			<img class="text-center" src="/images/angel.jpg" alt="angel" />
+		</div>
+		<p class="header2">Angel</p>
+		<p>Full blooded Spaniard, Angel is passionate about cultures and languages that led him to study and work as a tour guide in different countries. While abroad, he also worked in restaurants where got professional cooking knowledge. But long before that, he's always been a keen home cook and supporter of the Mediterranean diet and Spanish cuisine.</p>
+
+	</div>
+</div>
+
+
+@stop
