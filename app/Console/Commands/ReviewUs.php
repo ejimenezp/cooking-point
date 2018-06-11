0995a30d7d3fb8cd7dbeef86459f3dcf0fb3722c
ejimenezp <eduardo@cookingpoint.es>
2018-06-11 12:58:13 +0200

    new review us strategy
    based on weight defined in table crmstrategies

--- app/Console/Commands/ReviewUs.php
+++ app/Console/Commands/ReviewUs.php
@@ -3,17 +3,24 @@
 namespace App\Console\Commands;
 
 use App\Booking;
+use App\Crmstrategy;
 
 use \DateTime;
 use Log;
 
 use App\Http\Controllers\MailController;
+use App\Http\Controllers\CRMstrategyController;
 
 
 class ReviewUs extends Job {
 
+	private $strategy;
+	private $sent_so_far;
+
 	function __construct()
 	{
+		$this->strategy = Crmstrategy::orderBy('prio')->get();
+		$this->total_sent = Crmstrategy::where('prio', '>', '0')->sum('cummulated');
 		parent::__construct("<ReviewUs>");
 	}
 
@@ -32,20 +39,23 @@ class ReviewUs extends Job {
 
 	protected function action($bkg) {
 
-		switch ($bkg->source->name) {
-			case 'Viator':
-				Log::info('Enviado <ReviewUs on Viator> a ' . $bkg->name);
-			 	MailController::send_mail($bkg->email, $bkg, 'review_viator');
-				break;
-			
-			default:
-				Log::info('Enviado <ReviewUs> a ' . $bkg->name);
-			 	MailController::send_mail($bkg->email, $bkg, 'user_review');
-				break;
-		}
-	 	$bkg->crm = "REVIEW_ASKED";
-	 	$bkg->save();
+		// prio 0 is to force send out with associated template. They don't modify total_sent
 
+		$this->strategy->each(function ($item, $key) use ($bkg) {
+   			if (eval("return " . $item->check_expression . ";")) {
+   				if (($item->cummulated / max($this->total_sent, 1)) * 100 < $item->target_pct) {
+					MailController::send_mail($bkg->email, $bkg, $item->template);
+					Log::info("Enviado ReviewUs (" . $item->template . ") a " . $bkg->name);
+					// echo "Enviado ReviewUs " . $item->template . " a " . $bkg->name . "\r\n";
+					$this->total_sent++;
+					$item->cummulated++;
+					$item->save();
+					$bkg->crm = "REVIEW_ASKED";
+					$bkg->save();
+					return false;
+   				}
+   			}
+		});
 	}
 }
 
