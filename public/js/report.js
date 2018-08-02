17139627776a0f02433ea0cb4c376c5ab370d09e
Eduardo <eduardo@cookingpoint.es>
2018-08-02 10:36:38 +0200

    set datepicker end to end-of-month on reports

--- public/js/report.js
+++ public/js/report.js
@@ -29761,8 +29761,11 @@ jQuery(document).ready(function ($) {
 		defaultDate: date_shown.toDate(),
 		dateFormat: "DD, d MM yy",
 		altFormat: "yy-mm-dd",
-		altField: "#start"
-
+		altField: "#start",
+		onSelect: function onSelect() {
+			var selectedMoment = moment($('#start').val());
+			$('#enddatepicker').datepicker("setDate", selectedMoment.endOf('month').toDate());
+		}
 	});
 
 	$('#ui-datepicker-div').wrap('<div class="admin-eventdatepicker"></div>');
