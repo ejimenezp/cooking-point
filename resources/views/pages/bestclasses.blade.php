a44f079dc449ab743a12162e4af0bf12bf245654
Eduardo <eduardo@cookingpoint.es>
2018-08-09 12:57:14 +0200

    texts revised

--- resources/views/pages/bestclasses.blade.php
+++ resources/views/pages/bestclasses.blade.php
@@ -8,13 +8,13 @@
 
 @section('content')
 
-<h1 class="header1">Best Cooking Classes in Madrid</h1>
+<h1 class="header1">10 Reasons To Choose Us</h1>
 
-<p>These are some of the reasons why we believe our cooking classes to be the best in Madrid.</p>
+<p>Below, check outt en reasons why we believe us to be the best class in town.</p>
 
-<div class="header2">First Rated School in Madrid</div>
+<div class="header2">1. First Rated School in Madrid</div>
 
-<p>Since we started, back in 2013, our customer's 5-star reviews have granted us #1 position within our cateogory in <a href="https://www.tripadvisor.com/Attraction_Review-g187514-d4888426-Reviews-Cooking_Point-Madrid.html">TripAdvisor</a>, <a href="https://goo.gl/maps/ESe4TMXBok12">Google</a> and <a href="https://www.yelp.com/biz/cooking-point-madrid?sort_by=date_desc">Yelp</a>. We invite you to read their comments as we are sure they speak for themselves and they will for sure convince you that we are the right choice.</p>
+<p>Since we started, back in 2013, our customer's 5-star reviews have granted us #1 position within our cateogory in <a href="https://www.tripadvisor.com/Attraction_Review-g187514-d4888426-Reviews-Cooking_Point-Madrid.html">TripAdvisor</a>, <a href="https://goo.gl/maps/ESe4TMXBok12">Google</a> and <a href="https://www.yelp.com/biz/cooking-point-madrid?sort_by=date_desc">Yelp</a>. We invite you to read their comments as we are sure they speak for themselves.</p>
 
 <div class="row justify-content-center">
 
@@ -40,64 +40,61 @@
 </div>
 
 
-<div class="header2">100% Hands-on Classes</div>
+<div class="header2">2. 100% Hands-on Classes</div>
 
-<p>Our facilities are modern, confortable and luminous. We have several cooktops which are shared by two people each (don't worry if you come by yourself! We will try to find you a cooking mate). By following our instructors guidelines in English, you will perform all of the steps of each recipie. Everyone in the class performs the same tasks.</p>
+<p>Our facilities are modern, confortable and luminous. We have several cooktops which are shared by two people each (don't worry if you come by yourself! We will try to find you a cooking mate). You will perform all the steps of each recipe. Everyone in the class performs the same tasks.</p>
+
+<p>The maximum amount of customers per class is 12 but we are not usually fully booked. Of course, the class includes enjoying the dishes you prepare.</p>
+
+<p>And there's more! You will take home the recipe booklet to cook them for your family and friends!</p>
 
 <img src="/images/hands-on-class.jpg" class="img-fluid mx-auto d-block" alt="your spot">
 
-<p><br>The maximum amount of customers per class is 12 but we are not usually fully booked. Of course, the classs includes enjoying the dishes that you will have prepared.</p>
 
-<p>And there's more! You will take home the recipes booklet to repeat all the recipes for your family and friends!</p>
 
-<div class="header2">Suitable For Everyone</div>
+<div class="header2">3. Suitable For Everyone</div>
 
 <p>Our classes are suitable for everyone, you only need to be willing to learn, cook and meet other travellers.</p>
 
-<p>Having allergies or food restrictions is not a problem to attend our classes and make it a memorable experience. As we cook in pairs, your special requirements will not affect the rest of the customers, so feel free to specify your restrictions and we will adapt the recipes for you.</p>
+<p>Having allergies or dietary restrictions is not a problem to attend our class and make it a memorable experience. As you cook in pairs, your food requirements will not affect the rest of guests, so feel free to specify your needs and we will adapt the recipes for you.</p>
 
 <img src="/images/food-restrictions.jpg" class="img-fluid mx-auto d-block" alt="food restrictions">
 
-<p><br>It doesn't matter if you're travelling by yourself, in our classes you will find the ideal cooking partners to enjoy some hours immersed in Spanish gastronomy and culture.</p>
+<div class="header2">4. Family Friendly</div>
 
-
-<div class="header2">Family Friendly</div>
-
-<p>Our classes are suitable for everyone, you only need to be willing to learn, cook and meet other travellers.</p>
-
-<p>It doesn't matter if you're travelling by yourself or if you travel with kids, in our classes you will find the ideal cooking partners to enjoy some hours immersed in Spanish gastronomy and culture.</p>
+<p>Children love cooking, as well as our classes. Our kitchen is a nice setting where youngsters feel involved and enthusiastic about performing cooking tasks from simply stirring or grating to chopping vegetable and sauteing seafood.</p>
 
 <img src="/images/family-friendly-class.jpg" class="img-fluid mx-auto d-block" alt="family-friendly-class">
 
 <p></p>
 
-<div class="header2">100% Local</div>
+<div class="header2">5. 100% Local</div>
 
-<p>Through the stories told by our instructors, 100% born and raised in Spain, you will learn how to cook some of the most representative dishes of Spanish gastronomy. Also you will know first hand about traditions, habits and history behind all of them. Moreover, you will get insights and tips on how to understand food as a local and how to enjoy sharing our preparations.</p> 
+<p>We are a 100% Spanish company with all staff born and raised in Spain. We support small, traditional business and local economy by getting most of our supplies from the local market and using products with Protected Designation of Origin.</p> 
 
 <img src="/images/local-market.jpg" class="img-fluid mx-auto d-block" alt="local market">
 <p></p>
 
-<div class="header2">Right In The Center Of Madrid</div>
+<div class="header2">6. Right In The Center Of Madrid</div>
 
-<p>We are located in the center of the most historical area of Madrid, at a walking distance from the main lodging areas and turistic attractions. You will be able to come walking from your accomodation, enjoying the relaxed atmosphere of the Literary district.</p>
+<p>We are located in the historic center of Madrid, within walking distance of main lodging areas and turist attractions. You will probably be able to come walking from your accomodation, enjoying the relaxed atmosphere of the Literary Quarter.</p>
 
 <img src="/images/mapa-barrio-de-las-letras.jpg" class="img-fluid mx-auto d-block" alt="mapa Barrio de las Letras">
 
 <p></p>
-<div class="header2">Open All Year-round</div>
+<div class="header2">7. Open Year-round</div>
 <p>We are the school with more availability in Madrid. We offer 2 regular classes a day, 6 days a week, all year round.</p>
 
-<div class="header2">Legal</div>
-<p>We are a Company 100% Spanish, from Madrid, with all due licenses in force to teach cooking classes. In fact, we appear listed in the registry of activities recommended by the <a href="https://www.esmadrid.com/en/tourist-information/cooking-point">City of Madrid</a>'s official tourism website. We fulfill all of our legal obligations regarding health and safety, civil liability insurance and tax payment.</p> 
+<div class="header2">8. Legal</div>
+<p>We have all due licenses in force to teach cooking classes. In fact, we appear listed in the registry of activities recommended by the <a href="https://www.esmadrid.com/en/tourist-information/cooking-point">City of Madrid</a>'s official tourism website. We fulfill all of our legal obligations regarding health and safety, civil liability insurance and tax payment.</p> 
 
 <a href="https://www.esmadrid.com/en/tourist-information/cooking-point"><img src="/images/legal.jpg" class="img-fluid mx-auto d-block" alt="legal"></a>
 
-<div class="header2">What We Are Not</div>
-<p>We are not a franchise, there is only one Cooking Point and it is located in Madrid. We are not a restaurant or hotel teaching clandestine classes in a (most of the times) dark and unequiped kitchen. We are not impersonal resellers with fake photos and unefficient customer care.</p>
+<div class="header2">9. What We Are Not</div>
+<p>We are not a franchise, there is only one Cooking Point and it is in Madrid. We are not a restaurant or hotel teaching classes in small and messy kitchens. We are not impersonal resellers with fake photos and unefficient customer service.</p>
 
-<div class="header2">Best Value for Money</div>
-<p>Four hours of fun, no matter your choice.</p>
+<div class="header2">10. Best Value for Money</div>
+<p>Four hours of fun learning about Spain's culture.</p>
 <div class="row justify-content-center">
 	<div class="col-md-10 col-lg-8">
 		<div class="card-deck">
