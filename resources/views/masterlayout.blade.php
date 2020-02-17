23ab2ca4a77b408304e39ec55f0ef53956b897d9
Eduardo <eduardo@cookingpoint.es>
2020-02-17 19:23:35 +0100

    start using Google Tag Manager

--- resources/views/masterlayout.blade.php
+++ resources/views/masterlayout.blade.php
@@ -22,30 +22,16 @@
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
       @if (App::environment() == 'production')
-        <!-- Google Analytics -->
-        <script>
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-          ga('create', 'UA-43676257-1', 'auto');
-          @yield('analytics-ecommerce-tracking')
-          ga('send', 'pageview');
-        </script>
-        <!-- End Google Analytics -->
+        <!-- Google Tag Manager -->
+        @yield('analytics-ecommerce-tracking')
 
-        <!-- Global site tag (gtag.js) - Google AdWords: 985592263 -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-985592263"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'AW-985592263');
-        </script>
-        <!-- End Global site tag -->
-
-        @yield('adwords-event-snippet')
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K5H7TCB');</script>
+        <!-- End Google Tag Manager -->
 
       @else
         <!-- no analytics or crawling here. Testing environment -->
@@ -58,6 +44,10 @@
     
   <body>
 
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5H7TCB"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
 @section('footer')
 
