30d3680faa46283ca62716c217125620cdb2e1b4
Eduardo <eduardo@cookingpoint.es>
2020-05-08 10:58:10 +0200

    add ga script to booking page

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -4,7 +4,21 @@
 @section('description', 'Cooking Point booking form. Pay online and get instant confirmation.')
 
 @section('analytics-ecommerce-tracking')
-
+	@if ($tpv_result === 'OK' && ($bkg->calendarevent->type === 'PAELLA' || $bkg->calendarevent->type === 'TAPAS'))
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		dataLayer.push({
+		 'transactionId': '{{$bkg->locator}}',
+		 'transactionTotal': {{ round($bkg->price/1.21 , 2) }}, 
+		 'transactionProducts': [{
+		   'name': '{{ $bkg->calendarevent->type }}',
+		   'sku': '{{ $bkg->calendarevent->type }}',
+		   'price': {{ round($bkg->price/1.21 , 2) }},  
+		   'quantity':  1
+		 }]
+		});
+		</script>
+	@endif
 @stop
 
 
