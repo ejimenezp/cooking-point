69006e6c251621b53f1b7121ed7880f763d6cc07
Eduardo <eduardo@cookingpoint.es>
2020-09-17 16:45:10 +0200

    Create StripeController

---
+++ app/Http/Controllers/StripeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use Log;
+
+class StripeController extends Controller
+{
+<<<<<<< Updated upstream
+
+cambios en 5.0
+
+=======
+y estos cambios en prueba
+>>>>>>> Stashed changes
+
+}
